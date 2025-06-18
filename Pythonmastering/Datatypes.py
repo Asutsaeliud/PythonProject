acf2f1c0d77910fefe6761ed4884d980d61c4479
Asutsaeliud <asutsainzei@gmail.com>
2025-06-18 19:10:58 +0300

    initials files

---
+++ Pythonmastering/Datatypes.py
@@ -0,0 +1,29 @@
+
+number = 45 #integeri
+Second = 34.98 #float
+greeting = "Good morning" #string
+isPythonInteresting =True #boolean
+
+#Data Structure
+fruits = [ "apple", "banana", "cherry" ] # list
+cars = ( "Jeep", "BMW", "Tesla", "Toyota") #Tuple
+Countries = {"Kenya", "Maldives" ,"Canada"} #set
+Student = {
+    "firstname" :"Victory",
+    "lastname" :"Smith",
+"age":21,
+    "nationality":"Kenyan",
+} # Dictionary - key-value pair
+print(number)
+print(isPythonInteresting)
+print(fruits)
+print(cars)
+print(Countries)
+print(Student)
+print(Student["nationality"])
+
+#Typecasting - converting one data type to another
+print(float(number))
+print(int(Second))
+
+
---
+++ Pythonmastering/Introduction.py
@@ -0,0 +1,6 @@
+#Print is a method used to output a value
+
+print("Welcome to python programming language")
+print("Asutsa")
+print(100)
+print(10+20)
